d3e59a83eacd3a6412a13d61e71e07be2aed83d8
Bart <bart@vandelint.net>
2026-04-10 18:53:12 +0200

    Remove module docstring as it is in readme already

--- src/KiteUtils.jl
+++ src/KiteUtils.jl
@@ -1,17 +1,6 @@
 # SPDX-FileCopyrightText: 2022 Uwe Fechner
 # SPDX-License-Identifier: MIT
 
-"""
-    KiteUtils
-
-Utility functions for the kite simulators.
-
-This module provides data structures for the flight state and the flight log,
-functions for creating a demo flight state, demo flight log, loading and saving flight logs,
-functions for reading the settings, and helper functions for working with rotations.
-
-See https://ufechner7.github.io/KiteUtils.jl/stable/ for more information.
-"""
 module KiteUtils
 
 #= MIT License
