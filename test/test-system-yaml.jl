3c26dc2397f0e483fe8e554e35754a1d18fb7839
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-04-11 02:45:00 +0200

    Next try

--- test/test-system-yaml.jl
+++ test/test-system-yaml.jl
@@ -17,4 +17,7 @@ using KiteUtils, Test
     @test vsm_settings_file("system.yaml") == "vsm_settings.yaml"
     @test aero_geometry_file("system.yaml") == "aero_geometry.yaml"
     @test structural_geometry_file("system.yaml") == "struc_geometry.yaml"
+    @test vsm_settings("system.yaml") == "vsm_settings.yaml"
+    @test aero_geometry_settings("system.yaml") == "aero_geometry.yaml"
+    @test struc_geometry_settings("system.yaml") == "struc_geometry.yaml"
 end
