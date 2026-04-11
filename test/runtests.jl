1c87ab2a7672ee9f42888969fbcbf9a94dbb13f8
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-04-11 02:45:00 +0200

    Next try

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,36 +12,6 @@ Don't add your tests to runtests.jl. Instead, create files named
 The file will be automatically included inside a `@testset` with title "Title For My Test".
 =#
 
-@testset "KiteUtils.jl: system.yaml    " begin
-    # Ensure we're using the correct data path and file
-    set_data_path("data")
-    @test wc_settings("system.yaml") == "wc_settings.yaml"
-    @test fpc_settings("system.yaml") == "fpc_settings.yaml"
-    @test fpp_settings("system.yaml") == "fpp_settings.yaml"
-    @test vsm_settings("system.yaml") == "vsm_settings.yaml"
-    @test aero_geometry_settings("system.yaml") == "aero_geometry.yaml"
-    @test struc_geometry_settings("system.yaml") == "struc_geometry.yaml"
-end
-
-@testset "KiteUtils.jl: New Constructors" begin
-    se1 = Settings("system.yaml")
-    @test se1.sim_settings == "settings.yaml"
-    se2 = Settings("system_ram.yaml")
-    @test se2.model == "data/ram_air_kite_body.obj"
-    se2.model = "hey;)"
-    @test se1.model == "data/kite.obj"
-    @test se2.model == "hey;)"
-    @test se2.foil_file == "data/ram_air_kite_foil.dat"
-    se1.elevation = 420.0
-    se2.elevation = 11.11
-    @test se1.elevation == 420.0
-    @test se2.elevation == 11.11
-    dict1 = se_dict(se1)
-    dict2 = se_dict(se2)
-    dict1["initial"]["elevations"][1] == 420.0
-    dict2["initial"]["elevations"][1] == 11.11
-    
-end
 if basename(pwd()) == "test"; cd(".."); end
 
 for (_, _, files) in walkdir(@__DIR__)
--- test/test-system-yaml.jl
+++ test/test-system-yaml.jl
@@ -4,7 +4,15 @@
 using KiteUtils, Test
 
 @testset "KiteUtils.jl: system.yaml    " begin
+    cd(joinpath(@__DIR__, ".."))
+    set_data_path("data")
     @test wc_settings() == "wc_settings.yaml"
     @test fpc_settings() == "fpc_settings.yaml"
     @test fpp_settings() == "fpp_settings.yaml"
+    @test wc_settings("system.yaml") == "wc_settings.yaml"
+    @test fpc_settings("system.yaml") == "fpc_settings.yaml"
+    @test fpp_settings("system.yaml") == "fpp_settings.yaml"
+    @test vsm_settings("system.yaml") == "vsm_settings.yaml"
+    @test aero_geometry_settings("system.yaml") == "aero_geometry.yaml"
+    @test struc_geometry_settings("system.yaml") == "struc_geometry.yaml"
 end
